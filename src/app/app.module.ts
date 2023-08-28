494beefea6eae8c3f52fe9eb3bb64f006f13cb8d
mertesnn <mert_esen_@hotmail.com>
2023-08-28 17:36:14 +0300

    Main component changed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component'
+import { TodoComponent } from './todo/todo.component'
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [TodoComponent],
     imports: [BrowserModule],
     providers: [],
-    bootstrap: [AppComponent],
+    bootstrap: [TodoComponent],
 })
 export class AppModule {}
