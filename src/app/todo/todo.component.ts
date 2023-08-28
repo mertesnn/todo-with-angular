d13a6e9deb97ab56696f688c90a7c2b13773ccb3
mertesnn <mert_esen_@hotmail.com>
2023-08-28 18:11:36 +0300

    First design added.

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+    selector: 'app-todo',
+    templateUrl: './todo.component.html',
+    styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TodoComponent {
+    todos = [
+        { id: 1, description: 'Falan fistan', action: 'Yes' },
+        { id: 2, description: 'Falan fistan', action: 'No' },
+        { id: 3, description: 'asdasdadasd', action: 'No' },
+        { id: 3, description: 'Falasdasdan asdasdasd', action: 'Yes' },
+    ]
 }
